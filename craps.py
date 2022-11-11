3c21a7784a0d1a87706febf981075f700d2925ad
daa97 <114694180+daa97@users.noreply.github.com>
2022-11-11 12:59:18 -0500

    Odds method, betting started
    Implemented method to calculate dice roll odds
    Started interface for bet placement

--- craps.py
+++ craps.py
@@ -1,5 +1,5 @@
 from interface import App, EntryBox
-from random import randint
+from numpy.random import randint, multinomial
 
 def remove(string, chars):
     return "".join([c for c in filter(lambda x: not x in chars, string)])
@@ -14,33 +14,64 @@ def catch(err_func):
 
 
 class Dice:
-    def __init__(self):
+    "Represents a set of dice"
+    def __init__(self, count=2, sides=6):
+        self.count = count      # number of dice which are rolled
+        self.sides = sides      # number of sides per die
         self.total = None
         self.each = None
+        self.dist = self.pmf()
+
+    def chance(self, value):
+        return self.dist[value]
+
+
+    def pmf(self):
+        "returns a probability mass function for dice results"
+        p0 = [1]*self.sides                 # frequency of each dice roll for single die
+        pnew = p0                           # frequency of possible summed rolls for first die only
+        for die in range(1,self.count):     # iterate through each die which is rolled
+            pi = pnew                       # frequency for summed rolls of all previous dice
+            pnew = [0]*(self.sides*(die+1)-die) # initializes frequency for combination of all dice up to current
+            for roll in range(len(pi)):     # roll
+                for nextroll in range(len(p0)):
+                    pnew[roll+nextroll] += pi[roll]*p0[nextroll]
+        pdict = dict()
+        for i in range(len(pnew)):
+            pdict[i+self.count] = float(pnew[i]) / (self.sides**self.count)
+        return pdict        # returns dictionary containing pairs of (roll total):(probability)
     
+
     def roll(self):
-        self.each = (randint(1,6),randint(1,6))
+        "Rolls dice and returns the total value of the roll"
+        self.each = [randint(1,7) for i in range(self.count)]
         self.total = sum(self.each)
-        return self.total
+        return self.each
 
 class Table(Dice):
+    "Represents a craps table at which games take place"
     def __init__(self):
         super().__init__()
         self.point = False
 
-class Player(Table):
+class Player(Table): 
+    "Represents a single player at the table"
     def __init__(self):
         super().__init__()
         box = EntryBox("Please enter the balance you want to gamble:", check_func=lambda s: remove(s,"$,").isnumeric())
-        balance = int(remove(box.userval(),"$,"))
-        print(balance)
+        self.balance = int(remove(box.userval(),"$,"))
+        print(self.balance)
+
 class Bets(Player):
+    "Represents bets belonging to a particular player"
     def __init__(self):
         super().__init__()
+        self.pass_bet = 0
+        self.no_pass_bet = 0
+        self.odds_bet = 0
+        self.max_odds = 0
+    def pass_line(self, bet):
+        if not self.point:
+            bet = self.ingest_bet()
 
-
-def chance(roll):                   # returns the probability of a given roll (2-12) occuring
-    return (6 - abs(roll-7))/6**2   # rolls have 1/36, 2/36, 3/36 chance, with peak of 6/36 for a seven
-
-
-Player()
+App(Bets())
--- interface.py
+++ interface.py
@@ -1,24 +1,40 @@
 import tkinter as tk
 from tkinter import ttk
 
+
+
 class App(tk.Tk):
-    def __init__(self, player, table):
+    def __init__(self, bets):
         super().__init__()
+        self.bets = bets
+        self.balancelabel = ttk.Label(self,text=f"Balance: ${self.bets.balance}")
+        self.betlabel = ttk.Label(self,text="Last Bet: $0")
+        
+        self.betvar = tk.StringVar(value="0")
+        self.betbox = ttk.Entry(self,textvariable=self.betvar)
         self.dice = ttk.Frame(self)
-        self.dice.grid(row=0,column=0)
         self.die_images = [tk.PhotoImage(file=f"resources\\die_{i+1}.ppm") for i in range(6)]
         self.die1 = ttk.Label(self.dice, image=self.die_images[0])
         self.die2 = ttk.Label(self.dice, image=self.die_images[0])
         self.die1.grid(row=0, column=0)
         self.die2.grid(row=0, column=1)
-        self.roll_button = ttk.Button(self, text="Start Roll", command=table.roll_dice)
-        self.roll_button.grid(row=1, column=0)
 
+        self.roll_button = ttk.Button(self, text="Start Roll", command=self.roll_dice)
+
+        self.dice.grid(row=0,column=0, columnspan=2)
+        self.balancelabel.grid(row=1, column=0)
+        self.betlabel.grid(row=1, column=1)
+        self.betbox.grid(row=2,column=1)
+        self.roll_button.grid(row=3, column=0)
+        
         self.mainloop()
     
-    def update_dice(self, roll1, roll2):
-        self.die1['image'] = self.die_images[roll1 - 1]
-        self.die2['image'] = self.die_images[roll2- 1]
+
+    def roll_dice(self):
+        rolls = self.bets.roll()
+        print(rolls)
+        self.die1['image'] = self.die_images[rolls[0] - 1]
+        self.die2['image'] = self.die_images[rolls[1]- 1]
 
 
 class EntryBox(tk.Tk):
