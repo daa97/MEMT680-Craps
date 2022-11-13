8adb7ddd79e1a373378363ea1066902dffe57033
daa97 <114694180+daa97@users.noreply.github.com>
2022-11-13 10:45:27 -0500

    Betting & payouts added
    Game is now ~80% functional. Payouts added for pass/don't pass bets.
    Still to-do:
    - add odds bet payouts
    - remove bet from bankroll when first made
    - show total winnings in exit screen
    - keep winning bets on table (except with odds bet)

--- craps.py
+++ craps.py
@@ -3,7 +3,6 @@ from numpy.random import randint
 from interface import *
 
 
-
 def catch(err_func):
     def inner_func(err_func, *args, **kwargs):
         try:
@@ -19,7 +18,7 @@ class Dice:
         self.count = count      # number of dice which are rolled
         self.sides = sides      # number of sides per die
         self.total = None
-        self.each = None
+        self.each = [1]*count
         self.dist = self.pmf()
 
     def chance(self, value):
@@ -61,60 +60,153 @@ class Player(Table):
         self.name = EntryBox("Please enter your name:").userval()
         balance_box = EntryBox("Please enter the balance you want to gamble:", check_func=lambda s: remove(s,"$,0_ ").isnumeric())
         self.balance = int(remove(balance_box.userval(),"$,"))
-        print(self.balance)
+        self.starting_balance = self.balance
 
 class Bets(Player, App):
     "Represents bets belonging to a particular player"
     def __init__(self):
         #super().__init__()
-        Player.__init__(self)
-        App.__init__(self)
-        self.title(f"{self.name} plays craps")
-        self.pass_bet = 0
-        self.no_pass_bet = 0
-        self.odds_bet = 0
-        self.max_odds = 0
-        self.payout = 0
+        Player.__init__(self)                               # instantiate player
+        App.__init__(self)                                  # instantiate app
+        self.title(f"{self.name} plays craps")              # add window title
+        self.pass_bet = 0                                   # current wager on pass line
+        self.no_pass_bet = 0                                # current wager on don't pass line
+        self.odds_bet = 0                                   # current odds bet
+        self.max_odds = 0                                   # maximum allowable odds bet (dollar value)
+        self.payout = 0                                     # winnings from most recent roll
+        
+        # button configuration
+        self.pass_button.config(command=self.pass_line)     # call pass_line method on button click
+        self.no_pass_button.config(command=self.do_not_pass)# call do_not_pass method on button click
+        self.odds_button.config(command=self.odds)          # call odds method on button click
+        self.roll_button.config(command=self.shooter)       # call shooter method on button click
+        self.quit_button.config(command=self.on_close)      # call on_close method on button click
+        self.protocol("WM_DELETE_WINDOW", self.on_close)    # call on_close method when user attempts to "X" out of window
+        self.update_view()
         self.mainloop()
-
+    
     def pass_line(self):
+        "allows a pass line bet to be made"
         if not self.point:
             userval = self.ingest_bet()
             if userval is not None:
-                self.pass_bet = userval
+                self.pass_bet += userval
+
                 self.shooter()
-            
+
     def do_not_pass(self):
+        "allows a dont pass bet to be made"
         if not self.point:
             userval = self.ingest_bet()
             if userval is not None:
                 self.no_pass_bet = userval
                 self.shooter()
 
-    def shooter(self):
-        if max(self.pass_bet, self.no_pass_bet) != 0:
-            val = sum(self.roll())
-            if val==7 or val==11:
-                pass
+    def odds(self):
+        "allows an odds bet to be made"
+        if self.point:
+            userval = self.ingest_bet()
+            if userval is not None:
+                self.odds_bet = userval
+                self.shooter()
 
+    def shooter(self):
+        "rolls dice and evaluates payout"
+        self.payout = 0                     # set winnings from this particular roll
+        net_pass = self.pass_bet - self.no_pass_bet
+        if self.any_bet():
+            val = sum(self.roll())
+            if not self.point:              # if no point is set, in come-out phase
+                if val==7 or val==11:       # on 7 or 11, pass wins, dont pass loses
+                    self.payout = net_pass
+                elif val==2  or val==3:     # on 2 or 3, pass loses, dont pass wins
+                    self.payout = -net_pass
+                elif val==12:               # on 12, pass loses, dont pass ties
+                    self.payout = -self.pass_bet
+                else:                       # on other rolls, set point
+                    self.point = val
+                
+                if self.point:
+                    self.max_odds = (6 - abs(7-val)) * max(self.pass_bet, self.no_pass_bet)
+                else:
+                    self.clear_bets()
+                self.update_view()
+            
+            else:
+                if self.total==7:
+                    self.payout = -net_pass 
+                    self.clear_bets()
+                elif self.total == self.point:
+                    self.payout = net_pass
+                    self.clear_bets()
+                self.update_view() 
+    
+    def clear_bets(self):
+        "clear all existing bets"
+        self.balance += self.payout
+        self.point = 0
+        self.pass_bet = 0
+        self.no_pass_bet = 0
+        self.odds_bet = 0
+        self.max_odds = 0
+
+    def any_bet(self):
+        "Checks if any bet is currently on the table"
+        return max(self.pass_bet, self.no_pass_bet, self.odds_bet) != 0
+    
+    def update_view(self):
+        "Updates GUI view and options based on current status"
+        self.betvar.set("")
+        on = lambda tf: [("!" if tf else "") + "disabled"]      # translates boolean T/F into enabled/disabled status for widgets
+        self.pass_button.state(on(not self.point))              # pass line bet only enabled during come-out
+        self.no_pass_button.state(on(not self.point))           # dont pass bet only enabled during come-out
+        self.odds_button.state(on(self.any_bet()))              # odds bet only enabled when bet is on the table
+        self.quit_button.state(on(not self.any_bet()))          # quit button only enabled when no bets are on the table
+        
+        self.die[0]['image'] = self.die_images[self.each[0] - 1]
+        self.die[1]['image'] = self.die_images[self.each[1] - 1]
+        self.labels['balance'].config(text=f"Balance: ${self.balance:,}")
+        total_bet = self.pass_bet + self.no_pass_bet + self.odds_bet
+        self.labels['bet'].config(text=f"Bet: ${total_bet:,}")
+
+        if self.point:
+            self.labels['point'].configure(text=f"Point: {self.point}")
+        else:
+            self.labels['point'].configure(text=f"Point: None")
+    def on_close(self):
+        if self.any_bet():
+            messagebox.showerror(title="Not so soon", message="You still have bets on the table!")
+        else:
+            if messagebox.askyesno(title="Exit Game",
+            message="Are you sure you want to quit?"):
+                self.destroy()
+    
     def ingest_bet(self):
+        "Allows user to input a bet value"
         bet = remove(self.betvar.get(), "$,_ ")
-        if not bet.isnumeric():                     # check if bet contains non-numeric symbols
-            if remove(bet,".").isnumeric():         # check if bet is number with decimal
+        if not bet.isnumeric():                                 # check if bet contains non-numeric symbols
+            if remove(bet,".").isnumeric():                     # check if bet is number with decimal
                 messagebox.showerror(message="Bet must be whole dollar amount!")
-            elif remove(bet,"-").isnumeric():       # check if bet contains a minus sign
+
+            elif remove(bet,"-").isnumeric():                   # check if bet contains a minus sign
                 messagebox.showerror(message="Bet must be positive!")
-            else:                                   # if bet is not a number at all
+
+            else:                                               # if bet cannot be recognized as a number at all
                 messagebox.showerror(message="Bet must be a number!")
-        elif int(bet)<1:                            # check if bet is zero
+
+        elif int(bet)<1:                                        # check if bet is zero
             messagebox.showerror(message="Bet must be greater than zero!")
-        elif int(bet)>self.balance:                 # check if bet is greater than player balance
+
+        elif int(bet)>self.balance:                             # check if bet is greater than player balance
             messagebox.showerror(message=f'''You don't have enough money for that bet!
-                                            \nYour balance: ${self.bets.balance}
+                                            \nYour balance: ${self.balance}
                                             \nYou tried to bet: ${bet}
                                             \nWin a few more rounds first...''')
 
-        else:                                       # if bet is valid
+        elif self.point and int(bet)>self.max_odds:             # check if odds bet is greater than allowed max
+            messagebox.showerror(message=f"Odds bet is limited to ${self.max_odds}!")
+
+        else:                                       # if bet is valid, continue
             return int(bet)
 
 Bets()
--- interface.py
+++ interface.py
@@ -10,45 +10,53 @@ def remove(string, chars):
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.labelbox = ttk.Frame()
+        
+        self.labelbox = ttk.Frame(self)
         self.labels = dict()
         self.labels['title'] =  ttk.Label(self.labelbox,text=f"Craps", font=("bold 16"))
         self.labels['balance'] = ttk.Label(self.labelbox,text=f"Balance: $0")
         self.labels['bet'] = ttk.Label(self.labelbox,text="Current Bet: $0")
         self.labels['point'] = ttk.Label(self.labelbox, text="Point: None")
+        self.betvar = tk.StringVar(value="")
+        self.boxlabel = ttk.Label(self, text="Enter bet:  $")
+        self.betbox = ttk.Entry(self,textvariable=self.betvar, width=9)
         
-        self.betvar = tk.StringVar(value="0")
-        self.betbox = ttk.Entry(self,textvariable=self.betvar)
-
         # create frame containing images of two dice
         self.dice = ttk.Frame(self)
         self.die_images = [tk.PhotoImage(file=f"resources\\die_{i+1}.ppm") for i in range(6)]
         self.die = [ttk.Label(self.dice, image=self.die_images[0]), ttk.Label(self.dice, image=self.die_images[0])] 
         self.die[0].grid(row=0, column=0)
         self.die[1].grid(row=0, column=1)
-
-        self.pass_button = ttk.Button(self, text="Pass")
-        self.no_pass_button = ttk.Button(self, text="Do Not Pass")
-        self.roll_button = ttk.Button(self, text="Start Roll")
+        self.odds_button = ttk.Button(self, text="Odds Bet")        # button to make odds bet
+        self.pass_button = ttk.Button(self, text="Pass")            # button to make pass bet
+        self.no_pass_button = ttk.Button(self, text="Do Not Pass")  # button to make do not pass bet
+        self.roll_button = ttk.Button(self, text="Just Roll")       # button to roll without a bet
+        self.quit_button = ttk.Button(self,text="Quit Game")        # button to quit game
         
         # place all widgets in appropriate locations within window
+        
+        
         self.labels['title'].grid(row=0,column=0)
-        self.dice.grid(row=1,column=0, columnspan=2)
-        
-        self.labels["balance"].grid(row=3, column=0)
-        self.labels["bet"].grid(row=3, column=1)
-        self.labelbox.grid(row=0,column=0)
-        self.betbox.grid(row=4,column=1)
-        self.pass_button.grid(row=5, column=0)
-        self.no_pass_button.grid(row=5, column=1)
-        self.roll_button.grid(row=6, column=0)
-        
+        self.labels["balance"].grid(row=2, column=0)
+        self.labels["bet"].grid(row=3, column=0)
+        self.labels["point"].grid(row=4, column=0)
 
-    def update_view(self, roll, balance, point):
-        self.die[0]['image'] = self.die_images[roll[0] - 1]
-        self.die[1]['image'] = self.die_images[roll[1] - 1]
-        self.labels['balance'].configure(text=f"Balance: ${balance}")
-        self.labels
+        self.labelbox.grid(row=0,column=1, columnspan=2, padx=50)
+        self.dice.grid(row=1,column=1, columnspan=2)
+        self.boxlabel.grid(row=4, column=1, sticky="e")
+        self.betbox.grid(row=4,column=2, pady=5, sticky="w")
+        self.pass_button.grid(row=5, column=1)
+        self.no_pass_button.grid(row=5, column=2)
+        self.odds_button.grid(row=6,column=1)
+        self.roll_button.grid(row=6, column=2)
+        self.quit_button.grid(row=7,column=1,columnspan=2,pady=8)
+        self.columnconfigure(0, minsize=80, weight=1)
+        self.columnconfigure(3, minsize=80, weight=1)
+
+        
+    def clear_entry(self):
+        self.betvar.set("")
+        
 
 
 class EntryBox(tk.Tk):
@@ -65,10 +73,10 @@ class EntryBox(tk.Tk):
         self.box = ttk.Entry(self, textvariable=self.var)
         self.box.bind("<Return>", self.okay)
         self.button = ttk.Button(self,text="OK", command=self.okay)
-        self.label.grid(row=0,column=0)
-        self.box.grid(row=1,column=0)
+        self.label.grid(row=0,column=0, padx=15)
+        self.box.grid(row=1,column=0, padx=15)
         self.errlabel.grid(row=2, column=0)
-        self.button.grid(row=3,column=0)
+        self.button.grid(row=3,column=0, pady=15)
         self.mainloop()
 
     def userval(self):
@@ -78,7 +86,6 @@ class EntryBox(tk.Tk):
         if self.check_func is not None:
             if not self.check_func(self.var.get()):
                 self.errlabel['text']="Invalid Entry!"
-                
                 return
         self.destroy()
 
